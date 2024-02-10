1b3d5d56fc7cc31d060bb52872f5126db369cd2b
Alexin <alexindevs@gmail.com>
2024-02-10 11:44:49 +0100

    Added status for untracked files

--- git-commit-per-file.sh
+++ git-commit-per-file.sh
@@ -1,5 +1,5 @@
 git status -s | while read -r status file; do
-    if [ "$status" == "M" ] || [ "$status" == "A" ]; then
+    if [ "$status" == "M" ] || [ "$status" == "??" ] || [ "$status" == "A" ] || [ "$status" == "D" ]; then
         git add "$file"
         git commit -m "Update $file"
     fi
